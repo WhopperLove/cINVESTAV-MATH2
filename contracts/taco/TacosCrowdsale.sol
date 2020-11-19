086c0f816a21d19c2b0c520c9f335f041fe12303
WhopperLove <106108149+WhopperLove@users.noreply.github.com>
2020-11-19 16:06:11 +0000

    v0.7.4

--- contracts/taco/TacosCrowdsale.sol
+++ contracts/taco/TacosCrowdsale.sol
@@ -22,3 +22,30 @@ interface Pauseable {
  *      Hardcap = 210 ETH
  *      Once hardcap is reached:
  *        All liquidity is added to Uniswap and locked automatically, 0% risk of rug pull.
+ *
+ * @author soulbar@protonmail.com ($TEND)
+ * @author @Onchained ($TACO)
+ */
+contract TacosCrowdsale is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    //===============================================//
+    //          Contract Variables                   //
+    //===============================================//
+
+    // Caps
+    uint256 public constant ROUND_1_CAP = 70 ether; // CAP = 70
+    uint256 public constant ROUND_2_CAP = 140 ether; // CAP = 70
+    uint256 public constant ROUND_3_CAP = 210 ether; // CAP = 70
+    // HARDCAP = ROUND_3_CAP
+    
+    // During tests, we should use 12 ether instead given that by default we only have 20 addresses.
+    uint256 public constant CAP_PER_ADDRESS = 2 ether;
+    uint256 public constant MIN_CONTRIBUTION = 0.1 ether;
+
+    // Start time 08/09/2020 @ 6:00am (UTC) // For Cooks
+    uint256 public constant CROWDSALE_START_TIME = 1596952800;
+
+    // Start time 08/10/2020 @ 4:00pm (UTC)
+    uint256 public constant KARMASA
