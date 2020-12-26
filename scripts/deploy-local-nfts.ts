3975e32ce095e0b27165d9a45ee6fcf9ac8df2c0
WhopperLove <106108149+WhopperLove@users.noreply.github.com>
2020-12-26 14:19:18 +0000

    fixed it

--- scripts/deploy-local-nfts.ts
+++ scripts/deploy-local-nfts.ts
@@ -13,4 +13,24 @@ export async function main() {
   const TOTAL_SUPPLY = "15624000000000000000000000";
   const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
-  const [depl
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", await deployer.getAddress());
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // Set Uniswap Mock
+  const uniswapV2PairFactory = new UniswapV2PairMockFactory(deployer);
+  const uniswapPool = await uniswapV2PairFactory.deploy();
+  await uniswapPool.deployed();
+  console.log("\nUniswapV2Pair[Mock] deployed to:", uniswapPool.address);
+  const uniswapFactoryMockFactory = new UniswapFactoryMockFactory(deployer);
+  const uniswapFactory= await uniswapFactoryMockFactory.deploy(uniswapPool.address);
+  await uniswapFactory.deployed();
+  console.log("UniswapFactory[Mock] deployed to:", uniswapPool.address);
+
+  // Deploy Taco Token Contract
+  const tacoTokenFactory = new TacoTokenFactory(deployer);
+  const tacoToken = await tacoTokenFactory.deploy(TOTAL_SUPPLY, uniswapFactory.address, WETH_ADDRESS);
+
+  await tacoToken.deployed();
+  console.log("\nTacoToken deployed to:", tac
