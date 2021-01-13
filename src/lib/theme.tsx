3f57dc7694b3b95cf13a16086dccbf831572d1f9
WhopperLove <106108149+WhopperLove@users.noreply.github.com>
2021-01-13 05:53:46 +0000

    ajax endpoints

--- src/lib/theme.tsx
+++ src/lib/theme.tsx
@@ -20,4 +20,11 @@ const customTheme = {
     },
   },
   //   brand: {
-  
+  //     900: "#1a365d",
+  //     800: "#153e75",
+  //     700: "#2a69ac",
+  //   },
+  // },
+};
+
+export default customTheme;
---
+++ src/pages/api/tacos/index.ts
@@ -0,0 +1,4 @@
+import { tacos } from '../../../db/tacos';
+
+export default (req: any, res: any) => {
+  res.status(200).json(taco
