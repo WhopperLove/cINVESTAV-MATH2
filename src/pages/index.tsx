49b53c8149b60def99062f80a727fe934da70db0
WhopperLove <106108149+WhopperLove@users.noreply.github.com>
2021-01-28 06:28:01 +0000

    up version number

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,4 +13,41 @@ import LoadingTacos from "../components/LoadingTacos";
 const fetchInterval = 3000; // 3 seconds
 
 export type TaqueroStat = {
-  add
+  address: string;
+  timesCrunched: BigNumber;
+  tacosCrunched: BigNumber;
+  0: BigNumber;
+  1: BigNumber;
+};
+
+export type InfoFor = {
+  balance: BigNumber;
+  poolBalance: BigNumber;
+  totalSupply: BigNumber;
+  totalTacosCrunched: BigNumber;
+  crunchableTacos: BigNumber;
+  lastCrunchAt: BigNumber;
+  timesCrunched: BigNumber;
+  tacosCrunched: BigNumber;
+  tacoTuesday: boolean;
+  tacosCrunchRate: BigNumber;
+  taqueroRewardRate: BigNumber;
+  tacoTuesdayMultiplier: BigNumber;
+};
+
+function truncate(str, maxDecimalDigits) {
+  if (str.includes(".")) {
+    const parts = str.split(".");
+    return parts[0] + "." + parts[1].slice(0, maxDecimalDigits);
+  }
+  return str;
+}
+
+const truncateAddress = (str) => {
+  return str.slice(0, 5) + "..." + str.slice(38, 42);
+};
+
+function HomePage() {
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [isFetching, setIsFetching] = React.useState<boolean>(false);
+  const [isFetchingFirstTime, setIsFetchingFirstTime] = React.useState<bool
