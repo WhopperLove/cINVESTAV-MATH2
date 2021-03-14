0c0eddad0d7f6f6c952100324894ab11e6629138
WhopperLove <106108149+WhopperLove@users.noreply.github.com>
2021-03-14 15:49:58 +0000

    fix testing

--- test/pools/core/NFTStakeablePool.ts
+++ test/pools/core/NFTStakeablePool.ts
@@ -33,4 +33,31 @@ describe("NFTStakeablePool", function() {
     await underlyingToken.deployed();
 
     taconomics = await (new TaconomicsFactory(deployer)).deploy(
-      "0xa5409ec958c83c3f309868babaca7c86dcb077c1
+      "0xa5409ec958c83c3f309868babaca7c86dcb077c1",
+      "https://localhost:3000/tacos/",
+      "https://localhost:3000/contract/taconomics-erc1155"
+    );
+    await taconomics.deployed();
+    await taconomics.create(1, 0, []);
+
+    nftStakeablePool = await (new NftStakeablePoolFactory(deployer)).deploy(
+      "Test Pool",
+      taconomics.address,
+      underlyingToken.address,
+      "0x0000000000000000000000000000000000000000"
+    );
+    await nftStakeablePool.deployed();
+
+    await taconomics.addMinter(nftStakeablePool.address);
+  });
+
+  describe("#stake", function() {
+    it("Cannot stake before approving underlying", async function () {
+      await expect(nftStakeablePool.stake(100))
+        .to.be.revertedWith("ERC20: transfer amount exceeds allowance");
+    });
+
+    it("Cannot stake when underlying balance is 0", async function () {
+      await underlyingToken.connect(staker).approve(nftStakeablePool.address, 100);
+      await expect(nftStakeablePool.connect(staker).stake(100))
+        .to.be.rever
