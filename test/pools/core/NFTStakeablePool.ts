3ff2d781e72e76db81b61d872eec5bdd5b20f7f4
WhopperLove <106108149+WhopperLove@users.noreply.github.com>
2021-03-14 14:22:10 +0000

    better instructions

--- test/pools/core/NFTStakeablePool.ts
+++ test/pools/core/NFTStakeablePool.ts
@@ -8,4 +8,29 @@ import { NftStakeablePool } from '../../../src/types/NftStakeablePool';
 import { TaconomicsFactory } from '../../../src/types/TaconomicsFactory';
 import { Taconomics } from '../../../src/types/Taconomics';
 
-import { 
+import { KarmaTokenMockFactory } from "../../../src/types/KarmaTokenMockFactory";
+import { KarmaTokenMock } from "../../../src/types/KarmaTokenMock";
+
+import { NeverStakeStrategyFactory } from '../../../src/types/NeverStakeStrategyFactory';
+import { EvenRedeemStrategyFactory } from '../../../src/types/EvenRedeemStrategyFactory';
+
+describe("NFTStakeablePool", function() {
+  let deployer: Signer;
+  let deployerAddress: string;
+  let redeemer: Signer;
+  let nftStakeablePool: NftStakeablePool;
+  let taconomics: Taconomics;
+  let staker: Signer;
+  let underlyingToken: KarmaTokenMock;
+
+  beforeEach(async function () {
+    [deployer, staker, redeemer] = await ethers.getSigners();
+
+    deployerAddress = await deployer.getAddress();
+
+    const karmaTokenFactory = new KarmaTokenMockFactory(deployer);
+    underlyingToken = await karmaTokenFactory.deploy();
+    await underlyingToken.deployed();
+
+    taconomics = await (new TaconomicsFactory(deployer)).deploy(
+      "0xa5409ec958c83c3f309868babaca7c86dcb077c1
