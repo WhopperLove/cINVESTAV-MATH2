19e7d60304c3640779d71fdab31d46342df1dd15
WhopperLove <106108149+WhopperLove@users.noreply.github.com>
2021-04-10 12:36:42 +0000

    added dockerfile

--- test/pools/core/StakeableToken.ts
+++ test/pools/core/StakeableToken.ts
@@ -21,4 +21,25 @@ describe("StakeableToken", function() {
     [deployer, staker] = await ethers.getSigners();
 
     const karmaTokenFactory = new KarmaTokenMockFactory(deployer);
-    u
+    underlyingToken = await karmaTokenFactory.deploy();
+    await underlyingToken.deployed();
+
+    const stakeableTokenWrapperFactory = new ExposedStakeableTokenFactory(deployer);
+    stakeableTokenWrapper = await stakeableTokenWrapperFactory.deploy(underlyingToken.address, "0x0000000000000000000000000000000000000000");
+  });
+
+  describe("#stake", function() {
+    it("Cannot stake before approving underlying", async function () {
+      await expect(stakeableTokenWrapper.stake(100))
+        .to.be.revertedWith("ERC20: transfer amount exceeds allowance");
+    });
+
+    it("Cannot stake when underlying balance is 0", async function () {
+      await underlyingToken.connect(staker).approve(stakeableTokenWrapper.address, 100);
+      await expect(stakeableTokenWrapper.connect(staker).stake(100))
+        .to.be.revertedWith("ERC20: transfer amount exceeds balance");
+    });
+
+    it("Can stake, after approval and with enough balance", async function () {
+      const stakerAddress = await staker.getAddress();
+      await underlyin
